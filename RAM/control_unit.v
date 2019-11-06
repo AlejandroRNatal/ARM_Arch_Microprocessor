e2ce5fcbd11abccd2ec4004a007ebb8e94ebcb16
Ghoul <rodrigueznatalandy@gmail.com>
2019-11-06 10:53:11 -0400

    tester for CU alone, not integrated

--- RAM/control_unit.v
+++ RAM/control_unit.v
@@ -1565,7 +1565,7 @@ module mux_4_1(input [3:0]a , input [3:0]b, input [3:0]c, input [3:0]d,input sel
 
 endmodule
 
-module CU_tester;
+module CU_tester_with_ALU_and_REG;
 
 
     reg Done, Reset, Clk, Cond, Moc, RF_Ld;
@@ -1658,4 +1658,47 @@ module CU_tester;
 
 
 
+endmodule
+
+module CU_tester;
+    wire[5:0] State;
+    wire FR,RF,IR, MDR,MAR,R_W,MOV,MA_1,MA_0,MB_1,MB_0,MC_1,MC_0,MD, ME, OP4,OP3,OP2,OP1,OP0;
+    reg Moc, Clk, Reset, Done, Cond;
+
+    ControlUnit cu(State, FR,RF,IR, MDR,MAR,R_W,MOV,MA_1,MA_0,MB_1,MB_0,MC_1,MC_0,MD, ME, OP4,OP3,OP2,OP1,OP0,Moc, Cond,Done, Reset, Clk);
+
+    initial begin
+        State = 6'b000000;
+        Clk = 1'b0;
+        Cond =1'b0;
+        Moc = 1'b0;
+        //repeat(100) #5 State += 6'b000001;
+        repeat(100) #5 Clk = ~Clk;
+        repeat(100) #5 Moc = ~Moc;
+        repeat(100) #5 Cond = ~Cond;
+    end
+
+    initial fork
+        Reset = 1'b0;
+        #3 Reset = 1'b1;
+        #80 Reset = 1'b0;
+        #83 Reset = 1'b1;
+
+        Done = 1'b0;
+        #50 Done = 1'b1;
+        #60 Done = 1'b0;
+    join
+
+        initial begin
+            $display("===Control-Unit===\n");
+            Sdisplay("---Input---\n")
+            $display("FR | RF | IR | MDR | MAR | R_W | MOV | MA_1 | MA_0 | MB_1 | MB_0 | MC_1 | MC_0| MD | ME | OP4 | OP3 | OP2 | OP1 | OP0 | Time\n");
+            $monitor("%b | %b | %b | %b  | %b  | %b  | %b  |  %b  |  %b  |  %b  |  %b  | %b   |  %b | %b | %b | %b  | %b  | %b  | %b  |  %b |  %d",
+                     FR,RF,IR, MDR,MAR,R_W,MOV,MA_1,MA_0,MB_1,MB_0,MC_1,MC_0,MD, ME, OP4,OP3,OP2,OP1,OP0, Moc, Cond, Done, Reset, $time);
+            $display("---Ouput---\n");
+            $display("State | Moc | Cond | Done | Reset| Time\n");
+
+            $monitor("%d  | %b | %b | %b | %b |  %d",
+                    State  Moc Cond Done Reset $time);
+        end
 endmodule
